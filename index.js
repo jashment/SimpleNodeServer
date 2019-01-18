121bf4fc57a035fa5558d435fff22b6137ec975d
jashment <jordanashment@gmail.com>
2019-01-18 15:29:49 -0700

    Creating simple node server

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ const server = http.createServer((req, res) => {
         res.write('</html>')
         return res.end()
     }
+
+    if (req.url === '/api') {
+        const androidTypes = {
+            pie: 9,
+            oreo: 8,
+            nougat: 7
+        }
+        res.setHeader('Content-Type', 'application/json')
+        res.write(JSON.stringify(androidTypes))
+        return res.end()
+    }
+
+    if (req.url === '/user') {
+        const body = []
+        req.on('data', chunk => {
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            console.log(parsedBody.split('=')[1])
+        })
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        return res.end()
+    }
 })
 
 server.listen(5000)
